3f3b1c80ea3a3f63975ce09ec7302f79c4dc5404
MatthewT4 <matvey.tishin@yandex.ru>
2023-06-02 11:46:08 +0300

    simple async

--- async.go
+++ async.go
@@ -1,1 +1,20 @@
 package main
+
+import "fmt"
+
+func sum(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	c <- sum
+}
+
+func main() {
+	s := []int{11, 4, 222, 65}
+
+	c := make(chan int)
+
+	go sum(s, c)
+	fmt.Println(<-c)
+}
